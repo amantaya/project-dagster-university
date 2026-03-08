e9903685f7cc4b978501b114aaf506f493faa652
Andrew Antaya <andrew.antaya@sdstate.edu>
2026-03-08 21:57:25 +0000

    Refactor trip_update_job selection to exclude adhoc_request asset

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -11,8 +11,17 @@ weekly_update_job = dg.define_asset_job(
     selection=trips_by_week,
 )
 
+adhoc_request = dg.AssetSelection.assets(["adhoc_request"])
+
+adhoc_request_job = dg.define_asset_job(
+    name="adhoc_request_job",
+    selection=adhoc_request,
+)
+
 trip_update_job = dg.define_asset_job(
     name="trip_update_job",
     partitions_def=monthly_partition,  # partitions added here
-    selection=dg.AssetSelection.all() - trips_by_week
+    selection=dg.AssetSelection.all() - trips_by_week - adhoc_request,
 )
+
+
